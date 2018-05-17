b463abf0c69845d9a1a6d4ef1d8bd0a38e04a5b9
Hayden <33820650+sirredbeard@users.noreply.github.com>
2018-05-17 13:24:19 -0400

    Create install-anbox.sh

---
+++ install-anbox.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+##install kernel module dependencies
+
+sudo eopkg install git cmake pkg-config linux-headers
+
+##build kernel modules
+
+cd /tmp
+mkdir buildspace
+cd buildspace
+git clone --recursive https://github.com/anbox/anbox.git
+cd anbox/kernel
+
+	#build and install kernel modules
+	#apply kernel patches
+
+cd /tmp/buildspace/anbox
+
+##install anbox dependencies via eopkg
+
+sudo eopkg install lxc dbus dbus-devel protobuf protobuf-devel gtest-devel libboost libboost-devel libcap2-devel sdl2-devel sdl2-image-devel glib2-devel mesalib mesalib-devel
+
+##build and install anbox dependencies that are not in eopkg
+
+	#libproperties-cpp-dev
+
+		#get source
+		#build
+
+	#libglm-dev
+
+		#get source
+		#build
+
+	#gmock-devel
+
+		#get source
+		#build
+
+##build anbox
+
+cd /tmp/buildspace/anbox
+cmake
+cmake install
+
+##get android image
+
+cd /tmp/buildspace/
+wget https://build.anbox.io/android-images/2017/07/13/android_3_amd64.img
+mv android_3_amd64.img /usr/share/anbox/
+
+##generate .desktop file if necessary
