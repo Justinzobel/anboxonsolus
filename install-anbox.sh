fbac413d907e5c0c5e5d1bc7779bdf9d5069a4ba
Justin <justin.zobel@gmail.com>
2018-05-19 10:33:45 +0930

    Update install-anbox.sh
    some more progress, hopefully it helps

--- install-anbox.sh
+++ install-anbox.sh
@@ -2,7 +2,7 @@
 
 ##install kernel module dependencies
 
-sudo eopkg install make gcc g++ git cmake pkg-config linux-headers
+sudo eopkg install make gcc g++ git cmake pkg-config linux-current-headers libelf-devel
 
 ##build kernel modules
 
@@ -10,7 +10,33 @@ cd /tmp
 mkdir buildspace
 cd buildspace
 git clone --recursive https://github.com/anbox/anbox.git
-cd anbox/kernel
+##build/install binder
+cd anbox/kernel/binder
+make
+sudo make install
+##build/install ashmem
+cd ../ashmem
+make
+sudo make install
+
+#build/install gmock (required for dbus-cpp
+cd /tmp/buildspace
+mkdir gmock && cd gmock
+wget https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+tar xvf release-1.8.0.tar.gz
+cd googletest-release-1.8.0/googlemock
+cmake .
+make
+sudo make install
+# issue here is the gmock.pc (pkgconfig file) isn't generated
+
+cd /tmp/buildspace
+mkdir dbus-cpp && cd dbus-cpp
+wget https://launchpad.net/ubuntu/+archive/primary/+files/dbus-cpp_5.0.0+18.04.20171031.orig.tar.gz
+tar xvf dbus-cpp_5.0.0+18.04.20171031.orig.tar.gz
+wget https://aur.archlinux.org/cgit/aur.git/plain/boost-asio-1-66.patch?h=dbus-cpp -O boost-asio-1-66.patch
+patch -Np1 -i boost-asio-1-66.patch
+cmake .
 
 	#build and install kernel modules
 	#apply kernel patches
